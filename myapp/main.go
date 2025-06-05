e2733f0006106a688b88860a9327f0cb14687cad
MaximSau <clashminiclashmini016@gmail.com>
2025-06-05 19:18:38 +0300

    Update main.go

--- myapp/main.go
+++ myapp/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"html/template"
+	"myapp/db"
 	"myapp/handlers"
+	"myapp/models"
 	"path/filepath"
 
 	"github.com/labstack/echo/v4"
@@ -10,6 +12,12 @@ import (
 
 func main() {
 	e := echo.New()
+
+	if db.Err != nil {
+		panic("failed to connect database")
+	}
+	// Migrate the schema
+	db.Db.AutoMigrate(&models.User{})
 
 	// Подготовка шаблонов для `TemplateRenderer`
 	templatesGlobPattern := filepath.Join("templates", "*.html")
@@ -24,7 +32,9 @@ func main() {
 	// Устанавливаем `renderer` для Echo
 	e.Renderer = renderer
 
+	//url`s
 	e.GET("/", handlers.Home)
 	e.GET("/secondary/", handlers.SecPage)
+
 	e.Logger.Fatal(e.Start(":8000"))
 }
