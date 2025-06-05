385e2da29113e546a7f1303205777b740877316a
MaximSau <clashminiclashmini016@gmail.com>
2025-06-05 19:17:57 +0300

    Update user_model.go

--- myapp/models/user_model.go
+++ myapp/models/user_model.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"myapp/db"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name string
+	Age  uint
+	Work string
+}
+
+func NewUser(name, work string, age uint) {
+	db.Db.Create(&User{Name: name, Work: work, Age: age})
+}
